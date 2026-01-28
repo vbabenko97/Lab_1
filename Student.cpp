579dfd58d56d16d565452dfc468001b920e491a6
Vitalii Babenko <vbabenko2191@gmail.com>
2026-01-28 19:36:01 +0200

    Improve and translate README

--- Student.cpp
+++ Student.cpp
@@ -19,28 +19,35 @@ const std::string &Student::phone() const { return phone_; }
 const std::string &Student::faculty() const { return faculty_; }
 const std::string &Student::course() const { return course_; }
 
-void Student::setSurname(const std::string &surname) { surname_ = surname; }
-void Student::setName(const std::string &name) { name_ = name; }
-void Student::setPatronymic(const std::string &patronymic) {
-  patronymic_ = patronymic;
+void Student::setSurname(std::string surname) {
+  surname_ = std::move(surname);
 }
-void Student::setBirthday(const std::string &birthday) { birthday_ = birthday; }
-void Student::setAddress(const std::string &address) { address_ = address; }
-void Student::setPhone(const std::string &phone) { phone_ = phone; }
-void Student::setFaculty(const std::string &faculty) { faculty_ = faculty; }
-void Student::setCourse(const std::string &course) { course_ = course; }
+void Student::setName(std::string name) { name_ = std::move(name); }
+void Student::setPatronymic(std::string patronymic) {
+  patronymic_ = std::move(patronymic);
+}
+void Student::setBirthday(std::string birthday) {
+  birthday_ = std::move(birthday);
+}
+void Student::setAddress(std::string address) { address_ = std::move(address); }
+void Student::setPhone(std::string phone) { phone_ = std::move(phone); }
+void Student::setFaculty(std::string faculty) {
+  faculty_ = std::move(faculty);
+}
+void Student::setCourse(std::string course) { course_ = std::move(course); }
 
-void Student::print(std::ostream &out) const {
-  out << "\u0424\u0430\u043c\u0438\u043b\u0438\u044f: " << surname_ << '\n'
-      << "\u0418\u043c\u044f: " << name_ << '\n'
-      << "\u041e\u0442\u0447\u0435\u0441\u0442\u0432\u043e: " << patronymic_
-      << '\n'
-      << "\u0414\u0430\u0442\u0430 "
-         "\u0440\u043e\u0436\u0434\u0435\u043d\u0438\u044f: "
-      << birthday_ << '\n'
-      << "\u0410\u0434\u0440\u0435\u0441: " << address_ << '\n'
-      << "\u0422\u0435\u043b\u0435\u0444\u043e\u043d: " << phone_ << '\n'
-      << "\u0424\u0430\u043a\u0443\u043b\u044c\u0442\u0435\u0442: " << faculty_
-      << '\n'
-      << "\u041a\u0443\u0440\u0441: " << course_ << '\n';
+std::ostream &Student::print(std::ostream &out) const {
+  return out << *this;
+}
+
+std::ostream &operator<<(std::ostream &out, const Student &student) {
+  out << "Surname: " << student.surname() << '\n'
+      << "Name: " << student.name() << '\n'
+      << "Patronymic: " << student.patronymic() << '\n'
+      << "Date of birth: " << student.birthday() << '\n'
+      << "Address: " << student.address() << '\n'
+      << "Phone: " << student.phone() << '\n'
+      << "Faculty: " << student.faculty() << '\n'
+      << "Course: " << student.course() << '\n';
+  return out;
 }
